992bc0fdb68d198f60c4afa3d788ef6d7d37c00d
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-06-06 18:27:49 +0300

    create Profile model

--- projects/settings.py
+++ projects/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rates',
+    'bootstrap3',
+    'rest_framework',
+    'star_ratings',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +81,10 @@ WSGI_APPLICATION = 'projects.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'projects',
+        'USER': 'biron',
+    'PASSWORD':'Biron4745',
     }
 }
 
@@ -105,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
--- rates/models.py
+++ rates/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    bio = models.CharField(max_length=100)
+    profile_pic = models.ImageField(upload_to='images/', default='images/smoke.jpeg')
+    user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
+    
+    
+    
+    def save_profile(self):
+        self.save()
+        
+    def delete_profile(self):
+        self.delete()
+     
+    @classmethod   
+    def update_name(cls,id,new_First_Name):
+        cls.objects.filter(user_id = id).update(First_Name=new_First_Name)
+        new_title_object = cls.objects.get(First_Name=new_First_Name)
+        new_name = new_title_object.First_Name
+        return new_name
+        
+    @classmethod
+    def get_user_profile(cls,id):
+        profile = cls.objects.get(user_id=id)
+        return profile
+
+@receiver(post_save,sender=User)
+def create_profile(sender, instance,created,**kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    
+@receiver(post_save,sender=User)
+def save_profile(sender, instance,**kwargs):
+    instance.profile.save()
