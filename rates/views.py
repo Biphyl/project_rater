a9ca74a8a0e2900c8962cd8d83501a6e0e401f70
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-06-06 20:19:09 +0300

    create home function and base html

--- rates/views.py
+++ rates/views.py
@@ -1,2 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import *
+from .forms import ProjectUpload,UpdateProfileForm
+from django.contrib.auth.decorators import login_required
+from rest_framework import viewsets
+
+
+def home(request):
+    projects = Post.objects.all()
+    return render(request, 'projects/index.html', {"projects":projects})
+
+
 
