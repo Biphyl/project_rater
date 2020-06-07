8265b69b6fbb388a8564bbf6d104e4878a9609c2
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-06-07 07:31:23 +0300

    add new_project function

--- rates/views.py
+++ rates/views.py
@@ -10,4 +10,18 @@ def home(request):
     return render(request, 'projects/index.html', {"projects":projects})
 
 
+@login_required (login_url='/accounts/login/?next=/')   
+def new_project(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProjectUpload(request.POST,request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.user = current_user
+            project.save()
+        return redirect('home')
+    else:
+        form = ProjectUpload()
+        return render(request,'projects/new_post.html',{"form":form}) 
+
 
